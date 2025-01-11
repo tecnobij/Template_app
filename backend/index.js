6eadbcc2a8d307c6c2dcd99230eb3bbe9862d935
tecnobij <official.tecnobij@gmail.com>
2025-01-11 11:59:02 +0530

    backend file changed

--- backend/index.js
+++
@@ -1,9 +0,0 @@
-// Import required modules
-require('dotenv').config();
-const app = require('./app'); // Import app from app.js
-const connectToMongoDB = require('./src/config/db');
-
-// Initialize and configure environment variables
-connectToMongoDB();
-
-console.log('MongoDB connection initialized successfully.');
